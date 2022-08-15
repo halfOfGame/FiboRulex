45bf2a1cd0dc6adc3d11ad81a6c79d5df9cd4fd9
bruce\fibo-zhu <2547721127@qq.com>
2022-08-15 14:30:27 +0800

    更改 baseUrl

--- h5-auth-manager/baseUrl.js
+++ h5-auth-manager/baseUrl.js
@@ -5,26 +5,6 @@ switch (process.env.NODE_ENV) {
 	case 'development': // 开发环境代理地址
 		proxyObj = {
 			'/Riskmanage': {
-				target: 'http://47.102.125.25:80', // 开发环境
-				changeOrigin: true, // 是否跨域
-				pathRewrite: {
-					'^/Riskmanage': '/Riskmanage'
-				},
-
-			},
-			'/trading': {
-				target: 'http://47.242.85.45:80', // 开发环境
-				changeOrigin: true, // 是否跨域
-				pathRewrite: {
-					'^/trading': '/trading'
-				},
-
-			},
-		}
-		break
-	case 'test': // 测试环境代理地址
-		proxyObj = {
-			'/Riskmanage': {
 				target: 'http://localhost:8080', // 测试环境
 				changeOrigin: true, // 是否跨域
 				pathRewrite: {
@@ -33,57 +13,7 @@ switch (process.env.NODE_ENV) {
 			}
 		}
 		break
-	case 'release': // 军环境
-		proxyObj = {
-			'/Riskmanage': {
-				target: 'http://192.168.3.155:8080', // 谭环境
-				changeOrigin: true, // 是否跨域
-				pathRewrite: {
-					'^/Riskmanage': '/Riskmanage'
-				}
-			}
-		}
-		break
-	case 'niu': // 牛环境
-		proxyObj = {
-			'/Riskmanage': {
-				target: 'http://192.168.50.3:8080', // 生产环境
-				changeOrigin: true, // 是否跨域
-				pathRewrite: {
-					'^/Riskmanage': '/Riskmanage'
-				}
-			}
-		}
-		break
-	case 'jia': // 贾环境
-		proxyObj = {
-			'/Riskmanage': {
-				target: 'http://192.168.50.188:8080', // 生产环境
-				changeOrigin: true, // 是否跨域
-				pathRewrite: {
-					'^/Riskmanage': '/Riskmanage'
-				}
-			}
-			// '/list': {
-			// 	target: 'http://127.0.0.1:8888', // 生产环境
-			// 	changeOrigin: true, // 是否跨域
-			// 	pathRewrite: {
-			// 		'^/list': '/list'
-			// 	}
-			// },
-		}
-		break
-	case 'wang': // 汪环境
-		proxyObj = {
-			'/Riskmanage': {
-				target: 'http://192.168.50.228:8080', // 生产环境
-				changeOrigin: true, // 是否跨域
-				pathRewrite: {
-					'^/Riskmanage': '/Riskmanage'
-				}
-			}
-		}
-		break
+	
 }
 
 module.exports = proxyObj
